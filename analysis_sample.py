4fd5a5f1917e6947842bba85a715f6511ba69f63
Austin Fox <openafox@gmail.com>
2015-11-01 22:40:00 -0800

    add testing cases, fix some oop

--- analysis_sample.py
+++ analysis_sample.py
@@ -11,107 +11,120 @@ A -- apple
 import sys
 from basictools import get_nums
 
-class AnalysisSample(object):
-    """ Container for all analysis properties"""
 
-    def __init__(self):
-        # Take Off Angle
-        self.toa = self.get_toa
-        # List of accelerating voltages used on all elements
-        # gmrfilm defined for each
-        self.volts = []
-        get_nums('\tEnter Eo for element (def.=15'
-                                          '):', 58, 0, 15))
-        # List of film layers
-        self.layers = self.get_filmlayers()
+class AnalysisSample(object):
+    """ Container for all analysis properties
+    Keyword Arguments(give all or none):
+    toa -- Take Off Angle
+    volts -- list of accelerating voltages used
+    layers -- list of FilmLayer objects
+        (these contain an els attribute -- list of AtomicElement objects)
+    """
+
+    def __init__(self, **kwargs):
+        if len(kwargs) == 3:
+            # Take Off Angle
+            self.toa = kwargs['toa']
+            # List of accelerating voltages used on all elements
+            # gmrfilm defined for each
+            self.volts = kwargs['volts']
+            # List of film layers
+            self.layers = kwargs['layers']
+        else:
+            # Take Off Angle
+            self.toa = self.get_toa
+            # List of accelerating voltages used on all elements
+            # gmrfilm defined for each
+            self.volts = self.get_volts = []
+            # List of film layers
+            self.layers = self.get_layers([])
+
         # iteration vars
         self.i = 0
         self.j = 0
 
-
-    def get_filmlayers(self)
-        
-    def check_volts(self):
-        for el in self.layers:
-            if el.ecr <= self.volts*0.9:
-                #here#
+    def get_layers(self, layers=[]):
+        """Get new layers to add to the sample:
+        Returns list of FilmLayer objects.
+        """
+        while True:
+            layers.append(FilmLayer())
+            reply = yes_no('Add another layer?(Y/N):')
+            if reply is False:
                 break
-                print 'Overvoltage ratio for %s (%5.1f keV Ec) with Eo='
-                      '%5.1f keV is too low!\nTry Again:'
-                      % (els[i], ecr, e0[i])
+        return layers
+
+    def get_volts(self):
+        """Get accelerating voltages"""
+        volts = []
+        while True:
+            volts.append(get_nums('Accelerating Voltage [KeV]?:', 60, 0))
+            reply = yes_no('Add another voltage?(Y/N):')
+            if reply is False:
+                break
+        return voltages
+
+    def check_volts(self):
+        for layer, el in self:
+            while True:
+                if el.edge <= min(self.volts)*0.9:
+                    break
+                print ('Overvoltage ratio for %s (%5.1f keV Ec) with Eo='
+                       '%5.1f keV is too low!\nTry Again:'
+                       % (el.name, el.edge, min(self.volts)))
+                el.line = get_options('Lower Energy X-Ray Line for %s(%s)?'
+                                      '(Ka, Lg2, etc.):' % (el.name, el.line),
+                                      'lines')
+                el.setup_vars()
 
     def get_toa():
         """get take-off angle"""
         self.toa = get_nums('Enter take-off angle (default=40 degrees):',
                             90, 0, 40)
+
     def fixlayers():
-        
         for i in range(0, len(self.els)):
             if i > 0:
-                mess = '+Fix composition and thickness of layer %d? (def=n):' % i)
+                mess = ('Fix composition and thickness of layer %d? (def=n):'
+                        % i)
             if i == 0:
-                mess = '+Fix composition of substrate? (def=n):')
+                mess = '+Fix composition of substrate? (def=n):'
             ifix[i] = yes_no(mess, False)
 
-                    if i > 0:
-                        label = 'of layer %d' % i
-                    else:
-                        label = 'of the substrate'
-
-    # may be useful but add to 
-    def newline(nels, els, e0, lines):
-        """Queries operator for a new line for an element
-        because the old line was not excited at the new operating potential.
-
-        program completed 5/91 by richard a. waldo
-
-        Keyword Arguments:
-        nels -- Number of Elements
-        els -- List of element symbols
-        lines -- List of element lines used
-        e0 -- ??Original X-Ray energies list
-        Return:
-        ec -- Element Line X-Ray Energy [KeV]
-        lines -- updated
-        """
-        for i in range(0, nels):
-            (z, mass, ecr) = lookup(els[i], lines[i])
-            while True:
-                if ecr > e0[i]*0.9:
-                    lines[i] = get_options(
-                            'Overvoltage ratio for %s (%5.1f keV Ec) with Eo='
-                            '%5.1f keV is too low!\nEnter another line (e.g.'
-                            ' La1, Ka):' % (els[i], ecr, e0[i]), options, 'Ka')
-                elif ecr <= 0:
-                    lines[i] = get_options('Invalid line for this element;'
-                                        'try again:', options, 'Ka')
-                else:
-                    break
-                (z, mass, ecr) = lookup(els[i], lines[i])
-            ec[i] = ecr
-        return (ec, lines)
-
+            if i > 0:
+                label = 'of layer %d' % i
+            else:
+                label = 'of the substrate'
 
     def __iter__(self):
         return self
 
     def next(self):
-        nlays = len(self.layer)
-        lnel = len(self.layer[nlays-1].element)
+        nlays = len(self.layers)
+        lnel = len(self.layers[nlays-1].els)
         try:
-            layer = self.layer[self.i]
-            element = self.layer[self.i].element[self.j]
+            layer = self.layers[self.i]
+            el = self.layers[self.i].els[self.j]
         except IndexError:
             self.i = 0
             self.j = 0
             raise StopIteration
-        if self.j == len(self.layer[self.i].element) - 1:
+        if self.j == len(self.layers[self.i].els) - 1:
             self.i = self.i + 1
             self.j = 0
         else:
             self.j = self.j + 1
-        return layer, element
-    
+        return layer, el
+
 
 if __name__ == '__main__':
-    pass
+    from atomic_element import AtomicElement as AtEl
+    from film_layer import FilmLayer as FL
+    Si = AtEl('Si', 'Ka')
+    o = AtEl('O', 'Ka')
+    ti = AtEl('Ti', 'Ka')
+    layer1 = FL(els=[Si, o], rho=2.65)
+    layer2 = FL(els=[ti, o], rho=4.23)
+    sample = AnalysisSample(toa=40, volts=[15], layers=[layer1, layer2])
+    for lay, el in sample:
+        print 'Element:', el.name, ' Mass', el.mass
---
+++ film_layer.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+"""This is my doc string.
+
+Keyword arguments:
+A -- apple
+"""
+# Copyright 2015 Austin Fox
+# Program is distributed under the terms of the
+# GNU General Public License see ./License for more information.
+
+import sys
+from atomic_element import AtomicElement
+from basictools import get_nums, yes_no, user_alert, get_options, wtfract
+
+
+class FilmLayer(object):
+    """Container for properties specific to a Film Layer
+    Keyword Arguments:
+    els -- List of element objests
+    rho -- Layer Density"""
+
+    def __init__(self, **kwargs):
+        # Is Layer of known composition and thickness?
+        self.fix = False  # all layers are default not fixed
+        if 'els' in kwargs:
+            # List of atomic element objects in the film
+            self.els = kwargs['els']
+        else:
+            # List of atomic element objects in the film
+            self.els = self.get_els()
+
+        if 'rho' in kwargs:
+            # Layer Density
+            self.rho = kwargs['rho']
+        else:
+            # Layer Density
+            self.rho = self.get_dens()
+
+    def get_els(self, els=[]):
+        """Get new elements to add to the layer: Returns AtomicElement object.
+        """
+        while True:
+            els.append(AtomicElement())
+            els[-1].opt = AtomicElement.get_opt
+            reply = yes_no('Add another element?(Y/N):')
+            if reply is False:
+                break
+        return els
+
+    def get_dens(self):
+        """Gets estimated layer density from user.
+        Could make this a calc from xtal struct in future...
+        """
+        user_alert('Layer Density:\nUsed solely to convert thicknesses in '
+                   'ug/cm^2 to Angstroms; has no effect on the results.')
+        rho = get_nums('Approximate density (g/cm^3):', 25, 0)
+        return rho
+
+    def fixlayer(self):
+        """Get fixed values for  thickness and composition"""
+        # Get Thickness (layers not sub - sub has "infinite" thickness)
+        if not self.fix:
+            self.thick = 0.0
+            return False
+        self.units = get_options('+Enter fixed thicknesses in Angstroms (a)'
+                                 'or ug/cm**2 (u), (def=a):', ('A', 'U'), 'A')
+        if self.units == 'A':
+            label = 'Angstroms'
+        if self.units == 'U':
+            label = 'ug/cm**2'
+        self.thick = get_nums('Fixed thickness [%s]:' % label,
+                              10e5, 0, 0)
+        self.thick = self.thick/1.0e6   # g/cm**2
+        if self.units == 'A':
+                self.thick = self.thick*self.rho/100.  # cm
+
+        # Get Composition
+        if len(self.els) == 1:
+            # fraction in layer is one if only 1 element
+            self.els[0] = 1.0
+        else:
+            reply = get_options('Enter the fixed composition %s by '
+                                'weight fractions (w) or by atomic '
+                                'formula (def=a) :', ('W', 'A'), 'A')
+
+            for i in range(0, len(self.els)):
+                if reply == 'W':
+                    for j in range(0, self.els):
+                        self.els[j].wtfrac = get_nums(
+                                'Weight fraction concentration'
+                                'of element %s:' % self.els[j], 1.0, 0.0,
+                                default=1, zeroval=1.0e-9)
+                elif reply == 'A':
+                        self.els[j].atform = get_nums(
+                                'Atomic Formula of element %s '
+                                '(i.e 1 for Si in SiO2):' % self.els[j], 100)
+
+            if reply == 'A':
+                wtfracs = wtfract([el.name for el in self.els],
+                                  [el.atform for el in self.els])
+                for x, _ in enumerate(wtfracs):
+                    self.els[x].wtfrac = wtfracs[x]
+
+if __name__ == '__main__':
+    from atomic_element import AtomicElement as AtEl
+    Si = AtEl('Si', 'Ka')
+    o = AtEl('O', 'Ka')
+    layer = FilmLayer(els=[Si, o], rho=2.65)
+    print layer.els[0].mass
+    layer2 = FilmLayer()
