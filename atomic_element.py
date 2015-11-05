b447a56f9fee5f6a0f16a3495c09eeca1a4c25b3
Austin Fox <openafox@gmail.com>
2015-11-05 12:26:29 -0800

    effyld, get_rjump

--- atomic_element.py
+++ atomic_element.py
@@ -6,20 +6,16 @@ container for elements and their properties
 # Program is distributed under the terms of the
 # GNU General Public License see ./License for more information.
 
-from basictools import get_options, at_els, user_alert, get_data
+from basictools import get_options, at_els, user_alert, get_data, all_or_none
 
 
 class AtomicElement(object):
     """container for elements and their properties"""
 
     def __init__(self, name=None, line=None):
-        if name is None and line is None:
+        if all_or_none(name, line):
             # Elemet Name and Line
             self.name, self.line, self.z = self.get_el_nd_ln()
-        elif name is None or line is None:
-            # warn programer if improper usage
-            raise ValueError('Both or neither "name" and "line" must be '
-                             'specified.')
         else:
             self.name = name
             self.z = at_els.index(name.capitalize()) + 1
@@ -64,6 +60,11 @@ class AtomicElement(object):
         self.omega = self.get_omega()
         # Number of electrons in the ionized shell.
         self.znl = self.get_znl()
+        # JUMP Ratios
+        self.rjump = self.get_rjump()
+        # EFFective Fluorescence YieLD
+        self.effyld = self.get_effyld()
+
         with open('atomic_data.txt', 'r') as data_file:
             for row in data_file:
                 data = row.split("\t")
@@ -89,23 +90,29 @@ class AtomicElement(object):
                     # X-Ray Emmision Line Energy [KeV]
                     self.xray = xray if xray > 0.0 else 50000.0
 
-                    rjump = {'K': float(data[cols.index('rjump1')]),
-                             'L1': 1.17,
-                             'L2': 1.39,
-                             'L3': float(data[cols.index('rjump4')]),
-                             'M1': 1.16,
-                             'M2': 1.207,
-                             'M3': 1.158,
-                             'M4': 1.895,
-                             'M5': 1.895,
-                             'N': 2.0}
-                    # JUMP Ratios
-                    self.rjump = rjump[self.shell]
+    def get_rjump(self, z=None, shell=None):
+        if all_or_none(z, shell):
+            z = self.z
+            shell = self.shell
 
-    def get_omega(self):
+        rjump = {'K': get_data(z, 'rjump1'),
+                 'L1': 1.17,
+                 'L2': 1.39,
+                 'L3': get_data(z, 'rjump4'),
+                 'M1': 1.16,
+                 'M2': 1.207,
+                 'M3': 1.158,
+                 'M4': 1.895,
+                 'M5': 1.895,
+                 'N': 2.0}
+        return rjump[shell]
+
+    def get_omega(self, z=None, shell=None):
         """Fluorescence Yields"""
-        z = self.z
-        shell = self.shell
+        if all_or_none(z, shell):
+            z = self.z
+            shell = self.shell
+
         omega = 0.0
         if shell == 'K':  # Bambynek etal., Rev. Mod. Physics. 1972 pg 757
             # Could updat to Bambynek 1984
@@ -123,6 +130,62 @@ class AtomicElement(object):
             shell = shell.lower()
             omega = get_data(z, shell)
         return omega
+
+    def get_effyld(self, el=None, exciter='e'):
+        """EFFective Fluorescence YieLD for all lines.
+        3/91 r.a. waldo
+        NEED TO FIND REFS FOR THIS
+        """
+        if el is None:
+            el = self
+        # Coster-Kronig Coefficients
+        ck12 = get_data(el.z, 'c12')
+        ck13 = get_data(el.z, 'c13')
+        ck23 = get_data(el.z, 'c23')
+
+        if el.shell[0] != 'L':
+            effyld = el.omega
+        elif el.shell[0] == 'L':
+            rj1 = rjump(el.z, 2)
+            rj2 = rjump(el.z, 3)
+            rj3 = rjump(el.z, 4)
+            l1 = edge(el.z, 'Lb3')
+            l2 = edge(el.z, 'Lb1')
+            l3 = edge(el.z, 'La1')
+            if el.shell == 'L1':
+                effyld = el.omega
+            elif el.shell == 'L2':
+                if el.xray < l1 and el.xray > l2:
+                    effyld = el.omega
+                elif el.xray > l1:
+                    el1 = el
+                    el1.shell = 'L2'  # 3
+                    omegaL2 = self.get_omega(el1)
+                    if exciter == 'e':
+                        effyld = omegaL2*(1.0 + ck12)
+                    else:
+                        effyld = omegaL2*(1.0 + ck12*(rj1 - 1.0) *
+                                          rj2/(rj2 - 1.0))
+            elif el.shell == 'L3':
+                if el.xray < l2 and el.xray > l3:
+                    effyld = el.omega
+                elif el.xray < l1 and el.xray > l2:
+                    effyld = el.omega*(1.0 + ck23*(rj2 - 1.0) *
+                                       rj3/(rj3 - 1.0))
+                elif el.xray > l1:
+                    if exciter[0] == 'e':
+                        effyld = el.omega*(1.0 + 0.5*ck13 +
+                                           0.5*(1.0 + ck12)*ck23)
+                else:
+                    effyld = el.omega*(
+                            1.0 +
+                            ck13*(rj1 - 1.0)*rj2*rj3/(rj3 - 1.0) +
+                            ck23*(rj2 - 1.0)*rj3/(rj3 - 1.0) +
+                            ck12*(rj1 - 1.0)*rj2*rj3/(rj3 - 1.0))
+        elif el.shell[0] == 'N':
+            effyld = 0.0
+
+        return effyld
 
     def get_shell(self):
         """return shell for a line"""
@@ -165,6 +228,39 @@ class AtomicElement(object):
                 znl = 2
         return znl
 
+    def get_trnsprob(self, z=None, line=None):
+        """TRaNSition PROBability of line after shell ionization."""
+        if z is None:
+            z = self.z
+        if line is None:
+            line = self.line
+
+        if line[0] == 'K':
+            if z < 11.0:
+                trnsprob = 0.01
+            else:
+                trnsprob = 8.0e-3*(z - 10) - 8.75e-5*(z - 10)**2
+            if line == 'Ka':
+                trnsprob = 1.0 - trnsprob
+        elif line == 'Lg2':
+            trnsprob = 0.25
+        elif line == 'Lb3':
+            trnsprob = 0.45
+        elif line == 'Lb4':
+            trnsprob = 0.3
+        elif line == 'Lg1' or line == 'Lb1':
+            trnsprob = (z - 60.0)*0.001 + 0.14
+            if line == 'Lb1':
+                trnsprob = 1.0 - trnsprob
+        elif line == 'Lb2' or line == 'La1':
+            trnsprob = (z - 60.0)*0.001 + 0.16
+            if line == 'La1':
+                trnsprob = 1.0 - trnsprob
+        elif line[0] == 'M' or line[0] == 'N':
+            trnsprob = 1.0
+
+        return trnsprob
+
 if __name__ == '__main__':
     name = 'Mg'
     line = 'Ka'
@@ -175,6 +271,7 @@ if __name__ == '__main__':
     print 'absorption edge[kV]', el.edge
     print 'electron shell', el.shell
     print 'fluorescence yields', el.omega
+    print 'effective yield', el.effyld
     print 'number of electrons in the ionized shell', el.znl
     # print 'coster-kronig coefficients', el.ck
     # commented out for now, need to know how to choose which one
